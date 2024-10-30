872e10c29e3e74dc477c4c3ca65bd6dbe8bd04f4
wendzzoo <19844544551@163.com>
2024-10-30 15:15:23 +0800

    function

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -3,8 +3,17 @@
 
 int Multiply(int a, int b);
 
+void MultiplyAndLog(int a, int b) {
+	int result = Multiply(a, b);
+	std::cout << result << std::endl;
+}
+
 int main() {
 	Log("Hello World");
-	std::cout << Multiply(1, 2) << std::endl;
+
+	MultiplyAndLog(2, 4);
+	MultiplyAndLog(6, 9);
+	MultiplyAndLog(20, 43);
+
 	std::cin.get();
 }
